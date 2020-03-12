073ea348f9b0743d917a593e3b5b86b3fc7d5c6c
Nathaniel <nathaniel.parizi@enhanceit.us>
2020-03-12 13:40:47 -0400

    5

--- TestingAndEncryptionDemo/app/src/main/java/com/example/testingandencryptiondemo/KeyStoreWrapper.kt
+++ TestingAndEncryptionDemo/app/src/main/java/com/example/testingandencryptiondemo/KeyStoreWrapper.kt
@@ -1,2 +1,54 @@
 package com.example.testingandencryptiondemo
 
+import android.content.Context
+import android.icu.util.Calendar
+import android.security.KeyPairGeneratorSpec
+import java.math.BigInteger
+import java.nio.channels.NetworkChannel
+import java.security.KeyPair
+import java.security.KeyPairGenerator
+import java.security.KeyStore
+import java.security.PrivateKey
+import java.security.interfaces.RSAKey
+import javax.security.auth.x500.X500Principal
+
+const val PROVIDER_KEYSTORE = "AndroidKeyStore"
+
+class KeyStoreWrapper (val context : Context){
+
+    val keyStore = KeyStore.getInstance(PROVIDER_KEYSTORE).apply({load(null)})
+
+    //public and private keys
+    fun createKeyPair(alias : String) : KeyPair {
+
+
+
+        val generator = KeyPairGenerator.getInstance("RSA", PROVIDER_KEYSTORE)
+        val startDate = Calendar.getInstance()
+        val endDate = Calendar.getInstance().apply {startDate.add(Calendar.YEAR, 1)}
+
+        val generatedSpecs = KeyPairGeneratorSpec.Builder(context)
+            .setAlias(alias)
+            .setSerialNumber(BigInteger.ONE)
+            .setStartDate(startDate.time)
+            .setEndDate(endDate.time)
+            .setSubject(X500Principal("CN = ${alias} CA Certificate"))
+            .build()
+
+        generator.initialize(generatedSpecs)
+        return generator.generateKeyPair()
+    }
+
+    fun getKeyPair(alias : String) : KeyPair {
+
+        val privateKey = keyStore.getKey(alias,null) as PrivateKey
+        val publicKey = keyStore.getCertificate(alias).publicKey
+        return KeyPair (publicKey,privateKey)
+
+    }
+
+    fun removeKeys(alias : String){
+        keyStore.deleteEntry(alias)
+    }
+
+}
--- TestingAndEncryptionDemo/app/src/main/java/com/example/testingandencryptiondemo/MainActivity.kt
+++ TestingAndEncryptionDemo/app/src/main/java/com/example/testingandencryptiondemo/MainActivity.kt
@@ -2,17 +2,49 @@ package com.example.testingandencryptiondemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 
-
+const val TRANSFORMATION = "RSA/ECB/PKCS1Padding"
+const val ALIAS = "master"
 class MainActivity : AppCompatActivity() {
 
     // RoboElectric / Espresso
     // JUnit Mockito
 
+    val cipherWrapper = CipherWrapper(TRANSFORMATION)
+    val keyStoreWrapper = KeyStoreWrapper(applicationContext).apply{createKeyPair(ALIAS)}
+val keyPair = keyStoreWrapper.getKeyPair(ALIAS)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClick(view: View) {
+        when(view.id){
+            R.id.encryptBtn -> {encryptData()}
+            R.id.decryptBtn -> {decryptData()}
+        }
+    }
+
+    private fun encryptData() {
+
+    val stringToEncrypt = etEnterString.text.toString()
+        val encryptedString = cipherWrapper.ecryptData(stringToEncrypt, keyPair.public)
+        encrTxt.text = encryptedString
+
+    }
+
+    private fun decryptData() {
+
+        val stringToDecrypt = encrTxt.text.toString()
+        if(stringToDecrypt.isNotBlank()){
+            val decryptedString = cipherWrapper.decryptData(stringToDecrypt,keyPair.private)
+            dencrTxt.text = decryptedString
+        }
+
     }
 }
